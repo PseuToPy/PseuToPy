d7cae7675298941e809ee4eb3f176a97017c1d59
Patrick Wang <patrick.wang@isep.fr>
2020-04-30 00:49:54 +0200

    Implemented most of basic AST generators (#20)
    Implemented:
    - Variable declaration and assignments
    - Standard basic operations
    - `input` and `print` function calls
    - Function definitions and calls
    - Basic statements
    - Control structures
    - Declaration of data structures (list, tuple, dict, and set)

---
+++ pseutopy/generators/values.py
@@ -0,0 +1,39 @@
+import ast
+
+
+class Number(object):
+    def __init__(self, parent, value):
+        self.parent = parent
+        self.value = value
+
+    def to_node(self):
+        return ast.Num(n=self.value)
+
+
+class Name(object):
+    def __init__(self, parent, id):
+        self.parent = parent
+        self.id = id
+
+    def to_node(self):
+        return ast.Name(id="" + self.id, ctx=ast.Store())
+
+
+class String(object):
+    def __init__(self, parent, value):
+        self.parent = parent
+        self.value = value
+
+    def to_node(self):
+        return ast.Str(s=self.value)
+
+
+class NoneType(object):
+    def __init__(self, parent, value):
+        self.parent = parent
+        self.value = value
+
+    def to_node(self):
+        return ast.NameConstant(value="None")
+
+
