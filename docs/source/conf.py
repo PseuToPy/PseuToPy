670ec0b2a37ab875f0a0518843bf31fd05a715d7
Patrick Wang <patrick.wang@isep.fr>
2020-05-13 22:24:22 +0200

    Fixed issue with readthedocs compilation due to missing config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# Added reference to index as the master_doc to remove an issue with
+# readthedocs
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
