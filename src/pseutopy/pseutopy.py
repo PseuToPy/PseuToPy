8b19f951eb1f663d64fe3404b54904abeb880e27
Patrick Wang <patrick.wang@isep.fr>
2020-05-14 00:36:22 +0200

    Fixed issue with grammar path not being relative to PseuToPy class

--- src/pseutopy/pseutopy.py
+++ src/pseutopy/pseutopy.py
@@ -1,16 +1,16 @@
 import ast
+import os
 
-import astor
 from textx import metamodel_from_file
 
-from src.pseutopy.generators.expressions import Factor, UnaryOp, NotTest, \
+from pseutopy.generators.expressions import Factor, UnaryOp, NotTest, \
     BinaryOp, OrTest, AndTest, Comparison, TestList, Expr, XorExpr, AndExpr, \
     ShiftExpr, ArithExpr, Term, Power, TestListStarExpr, AtomExpr, Atom, \
     Parameters, TypedArgsList
-from src.pseutopy.generators.statements import Statement, ExprStmt, \
+from pseutopy.generators.statements import Statement, ExprStmt, \
     InputStmt, FuncCallStmt, DeclareStmt, PrintStmt, DelStmt, IfStmt, \
     WhileStmt, ForStmt, FuncDef, ReturnStmt
-from src.pseutopy.generators.values import Number, Name, String, NoneType, \
+from pseutopy.generators.values import Number, Name, String, NoneType, \
     Boolean
 
 
@@ -18,7 +18,7 @@ class PseuToPy(object):
     def __init__(self):
         self.python_ast = ast.Module(body=[])
         self.variables = []
-        self.meta_model = metamodel_from_file('src/pseutopy/pseudocode.tx',
+        self.meta_model = metamodel_from_file(os.path.dirname(__file__) + "/pseudocode.tx",
                                               debug=False,
                                               classes={DeclareStmt, InputStmt,
                                                        PrintStmt, DelStmt,
@@ -74,91 +74,3 @@ class PseuToPy(object):
             return ast.Break()
         if stmt == 'continue':
             return ast.Continue()
-
-
-def main():
-    pseutopy = PseuToPy()
-    model = pseutopy.convert_from_string("""
-    set a, b to 1 + 2 - True * a < b, 3
-    set c to ()
-
-    input("Hello")
-
-    set foo to None
-    a = 1
-    declare bix
-
-    for i in range(0):
-        for j in range(0, 10):
-            for k in range(0, 10, 1):
-                set a to None
-                declare b
-                declare c
-                declare d
-                print(1, "Hello", True)
-                print(1 + True)
-                print "Hello" + "world"
-                print "Hello" followed by a followed by "my friend" + "Bob"
-                if a == b:
-                    set a to b
-                end
-            end
-        end
-    end
-
-    if a is greater than b then
-        if b is lower than c then:
-            if d is lower than e then     :
-                set f to the result of input integer ("Hi")
-                set b to the result of input number("hello")
-                set a to the result of input("hello")
-                if e is equal to f:
-                    a = 2
-                    b = 3
-                end
-            end
-        end
-    end
-
-    if True:
-        a = 1
-    else if True:
-        b = 2
-    elif True:
-        c = 3
-    else if True:
-        d = 4
-    else:
-        e = 5 to the power of 2
-    end
-
-    def fizz(a):
-        return None
-    end
-    def fizz():
-        return 1
-    end
-    def fizz with no parameter to do:
-        print("Hello")
-    end
-    define function fizz with a as parameter to do:
-        return a
-    end
-    define function fizzbuzz with a, b as parameters:
-        return b
-    end
-    define function fizzbuzz with (a,b,c) as parameters to do  :
-        return 1
-    end
-
-    call function fizzbuzz
-    call function fizzbuzz with parameter a
-    call function fizzbuzz with parameters a, b
-    set a to (a, b, c)
-    set a to the result of call function fizzbuzz with parameters a, b
-    """)
-    print(astor.to_source(model))
-
-
-if __name__ == '__main__':
-    main()
