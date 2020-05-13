6778bce1d16fd6c0b1aa5625a3ccc73aaeb6868a
Patrick Wang <patrick.wang@isep.fr>
2020-05-13 21:37:38 +0200

    Added CLI tool to test PseuToPy (#28)
    - Took the CLI tool developed initially on the other repository
    - Tested the feature with both a string input and a file input

---
+++ pseutopy.py
@@ -0,0 +1,36 @@
+import argparse
+import ast
+import astor
+import pseutopy
+
+parser = argparse.ArgumentParser(description='A pseudocode to Python '
+                                             'converter written in Python '
+                                             'using textX.')
+parser.add_argument('input',
+                    help="Pseudocode input to be converted into Python")
+input_group = parser.add_mutually_exclusive_group()
+input_group.add_argument('-f', '--file', action='store_true',
+                         help="Input is now expected to be a file")
+input_group.add_argument('-s', '--string', action='store_true',
+                         help="Input is now expected to be a string (default)")
+parser.add_argument('-a', '--ast', action='store_true',
+                    help="Prints out the generated Python AST")
+parser.add_argument('-q', '--quiet', action='store_true',
+                    help="Don't print the generated Python code")
+# TODO: Modify this CLI tool to define the language of the grammar
+args = parser.parse_args()
+
+pseutopy = pseutopy.pseutopy.PseuToPy()
+if not args.file:
+    generated_ast = pseutopy.convert_from_string(args.input)
+else:
+    generated_ast = pseutopy.convert_from_file(args.input)
+
+generated_code = astor.to_source(generated_ast)
+
+if args.ast:
+    print(ast.dump(generated_ast))
+if not args.quiet:
+    print(generated_code)
+# if args.exec:
+#     exec(generated_code)
