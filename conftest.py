9d396ad6af2b20ec3f7d3cc09f37681bc6b5c7bc
guillaume rouaix <guillaumerouaix@gmail.com>
2020-09-20 20:11:41 +0200

    reso erreur tests en

--- conftest.py
+++ conftest.py
@@ -4,12 +4,12 @@ from pseutopy.pseutopy import PseuToPy as oldPseuToPy
 from src.pseutopy.pseutopy import PseuToPy
 
 @pytest.fixture(scope="session")
-def pseutopy():
+def enPseutopy():
     return oldPseuToPy()
 
-@pytest.fixture(scope="session")
-def enPseutopy():
-    return PseuToPy("en")
+#@pytest.fixture(scope="session")
+#def nPseutopy():
+#    return PseuToPy("en")
 
 @pytest.fixture(scope="session")
 def frPseutopy():
