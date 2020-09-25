9f1617013856df8d81b8e05703bfeb0032f36f78
tudo_bem <33052345+guillaumerouaix@users.noreply.github.com>
2020-09-25 10:55:28 +0200

    modification cli -> -type: string -language: fr ''

--- cli.py
+++ cli.py
@@ -11,10 +11,10 @@ parser = argparse.ArgumentParser(description='A pseudocode to Python '
 parser.add_argument('input',
                     help="Pseudocode input to be converted into Python")
 input_group = parser.add_mutually_exclusive_group()
-parser.add_argument('-s-language:', '-string-language:', '-string-lg:', '-s-lg:', action='store', dest='string',
-                    help='Input is now expected to be a string (default)')
-parser.add_argument('-f-language:', '-file-language:', '-file-lg:', '-f-lg:', action='store', dest='file',
-                    help='Input is now expected to be a file (default)')
+parser.add_argument('-type:', action='store', dest='type',
+                    help='Input is now expected to be a "string" or "file"')
+parser.add_argument('-language:', '-lg:', action='store', dest='language',
+                    help='Input is now expected to be "fr" or "en"')
 parser.add_argument('-a', '--ast', action='store_true',
                     help="Prints out the generated Python AST")
 parser.add_argument('-q', '--quiet', action='store_true',
@@ -22,11 +22,11 @@ parser.add_argument('-q', '--quiet', action='store_true',
 # TODO: Modify this CLI tool to define the language of the grammar
 args = parser.parse_args()
 
-if not args.file:
-    pseutopy = PseuToPy(args.string)
+if args.type in ['s', 'string']:
+    pseutopy = PseuToPy(args.language)
     generated_ast = pseutopy.convert_from_string(args.input)
 else:
-    pseutopy = PseuToPy(args.file)
+    pseutopy = PseuToPy(args.language)
     generated_ast = pseutopy.convert_from_file(args.input)
 
 generated_code = astor.to_source(generated_ast)
