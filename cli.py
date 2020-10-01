a3ab766df8aa494857f7c8bf44b18685a3a955a5
guillaume rouaix <guillaumerouaix@gmail.com>
2020-10-01 07:52:40 +0200

    modif cli

--- cli.py
+++ cli.py
@@ -11,15 +11,15 @@ parser = argparse.ArgumentParser(description='A pseudocode to Python '
 parser.add_argument('input',
                     help="Pseudocode input to be converted into Python")
 input_group = parser.add_mutually_exclusive_group()
-parser.add_argument('-type:', action='store', dest='type',
+parser.add_argument( '-t', '--type', action='store', dest='type',
                     help='Input is now expected to be a "string" or "file"')
-parser.add_argument('-language:', '-lg:', action='store', dest='language',
+parser.add_argument( '-l', '--language',action='store', dest='language',
                     help='Input is now expected to be "fr" or "en"')
 parser.add_argument('-a', '--ast', action='store_true',
                     help="Prints out the generated Python AST")
 parser.add_argument('-q', '--quiet', action='store_true',
                     help="Don't print the generated Python code")
-# TODO: Modify this CLI tool to define the language of the grammar
+
 args = parser.parse_args()
 
 if args.type in ['s', 'string']:
