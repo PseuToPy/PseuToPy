f7ca31a6ed102d944bc1317972b653f20d0e9317
hmatico <mathieu.valentin1@orange.fr>
2021-05-06 11:20:44 +0200

    [PTP-005] Adding tests for function and async function

---
+++ tests/test_funcdef.py
@@ -0,0 +1,34 @@
+"""
+Current state of funcdef rule: 
+
+async_funcdef: "async" funcdef
+funcdef: ("def" | "define" "function") NAME "(" parameters? ")" ["->" test] (":" | "to do" | "to do:") suite
+"""
+
+def test_func_async(pseutopy):
+    template = '''
+async {} my_func(){} print("")
+    '''
+    python_code = template.format('def',':')
+    pseutopy_code = [template.format('define function',' to do'), template.format('def',':'), template.format('def',' to do:')]
+    for i in range(len(pseutopy_code)):
+        assert pseutopy.convert_from_string(python_code) == pseutopy.convert_from_string(pseutopy_code[i])
+
+def test_func_1(pseutopy):
+    template = '''
+{} my_func(a,b){} print(a*b)
+    '''
+    python_code = template.format('def',':')
+    pseutopy_code = [template.format('define function',' to do'), template.format('def',':'), template.format('def',' to do:')]
+    for i in range(len(pseutopy_code)):
+        assert pseutopy.convert_from_string(python_code) == pseutopy.convert_from_string(pseutopy_code[i])
+
+def test_func_2(pseutopy):
+    template = '''
+{} my_func(a,b){}(2 * a + 3) % 5 == 0{} print(a*b)
+    '''
+    python_code = template.format('def','->',':')
+    pseutopy_code = [template.format('define function','->',' to do'), template.format('def','->',':'), 
+    template.format('def','->',' to do:')]
+    for i in range(len(pseutopy_code)):
+        assert pseutopy.convert_from_string(python_code) == pseutopy.convert_from_string(pseutopy_code[i])
