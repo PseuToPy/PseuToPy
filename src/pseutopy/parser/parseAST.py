d502fd516db0cdd3e0b4f9ec9f8d93247d91531b
Laurent Yu <laurentyu1997@hotmail.fr>
2021-05-20 17:09:09 +0200

    [PTP-008] try mapping

--- src/pseutopy/parser/parseAST.py
+++ src/pseutopy/parser/parseAST.py
@@ -1,11 +1,24 @@
 from src.pseutopy.pseutopy import PseuToPy as pseutopy
 import ast
 
-test = 'a = 1 + 2'
+test = 'a = 1'
 
-mapping = {
 
-}
-tree_ast = pseutopy().parser.parse(test + '\n')
+def switcher(index, children):
+    mapping = {
+        'file_input': parser(children[0]),
+        'assign': ast.Assign(parser(children[0]), parser(children[1])),
+        'var': ast.Name(children[0].val),
+        'number': ast.Num(children[0].val),
+    }
+    return mapping[index]
+
+
+def parser(tree):
+    return switcher(tree.data, tree.children)
+
+
+tree_ast = pseutopy().parser.parse(test + '\n').children[0]
+parser(tree_ast)
 
 print(tree_ast.pretty())
